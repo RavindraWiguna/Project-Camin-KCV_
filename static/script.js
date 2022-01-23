a14c61c1322136d7d9f80db689d33728db7d90b6
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-01-24 06:59:52 +0700

    clean javascript

--- static/script.js
+++ static/script.js
@@ -1,15 +1,3 @@
-// const image_input = document.querySelector("#img-input");
-// var uploaded_image = "";
-var constraints = { video: { facingMode: "user" }, audio: false };
-// image_input.addEventListener("change", function(){
-//     const reader = new FileReader();
-//     reader.addEventListener("load", () => {
-//         uploaded_image = reader.result;
-//         document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`
-//     });
-//     reader.readAsDataURL(this.files[0]);
-// })
-
 const stopCamButton = document.getElementById("stop-cam");
 if(!!stopCamButton){
     stopCamButton.addEventListener('click', function(){
