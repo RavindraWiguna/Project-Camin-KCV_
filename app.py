bbe3ee45dd01c1a5b0d505ef10dc6decf39f8d82
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-01-23 20:20:48 +0700

    work locally sad

--- app.py
+++ app.py
@@ -1,16 +1,71 @@
-from os import name
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template, request, Response
+import cv2
 
 app = Flask(__name__)
 
+#LOGIC AND OPEN CV FUNC
+listCam = []
+didCheck = False
+camId = 0
+
+def change():
+    # print("changing")
+    global camId
+    camId+=1
+    camId %= len(listCam)
+
+def generateFrame():
+    try:
+        cap = cv2.VideoCapture(listCam[camId])
+    except IndexError:
+        print(f'cam list len: {len(listCam)}, camId: {camId}')
+        cap = cv2.VideoCapture(0)
+        if(cap.isOpened()):
+            print("ABLE TO OPEN")
+        else:
+            print("cant even run capture 0")
+    while True:
+        success, frame = cap.read()
+        if(not success):
+            break
+        else:
+            '''did tensorflow here'''
+            
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+
+            yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    global didCheck
+    if(not didCheck):
+        for i in range(0, 10):
+            cap = cv2.VideoCapture(i)
+            if(not cap.isOpened()):
+                continue
+            listCam.append(i)
 
-@app.route("/<address>/")
-def any(address):
-    return render_template("index.html", isi=address)
+    # print(listCam)
+    return render_template("home.html")
+
+@app.route("/detect/", methods=["POST", "GET"])
+def detect():
+    if(request.method=='POST'):
+        if("changeCam" in request.form):
+            change()
+    return render_template("detect.html")
+
+
+@app.route('/video')
+def video():
+    return Response(generateFrame(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == "__main__":
+    # getAvailCam()
     app.run(debug=True)
 
--- static/script.js
+++ static/script.js
@@ -1,11 +1,31 @@
-const image_input = document.querySelector("#img-input");
-var uploaded_image = "";
+// const image_input = document.querySelector("#img-input");
+// var uploaded_image = "";
+var constraints = { video: { facingMode: "user" }, audio: false };
+// image_input.addEventListener("change", function(){
+//     const reader = new FileReader();
+//     reader.addEventListener("load", () => {
+//         uploaded_image = reader.result;
+//         document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`
+//     });
+//     reader.readAsDataURL(this.files[0]);
+// })
 
-image_input.addEventListener("change", function(){
-    const reader = new FileReader();
-    reader.addEventListener("load", () => {
-        uploaded_image = reader.result;
-        document.querySelector("#display-image").style.backgroundImage = `url(${uploaded_image})`
-    });
-    reader.readAsDataURL(this.files[0]);
-})
+const stopCamButton = document.getElementById("stop-cam");
+if(!!stopCamButton){
+    stopCamButton.addEventListener('click', function(){
+        // alert("going to '' ");
+        location.href = "/";
+        
+    })
+}
+
+
+const startCamButton = document.getElementById("start-cam");
+if(!!startCamButton){
+    startCamButton.addEventListener('click', function(){
+        // alert("starting camera");
+        location.href = "/detect";
+    })
+}
+
+
