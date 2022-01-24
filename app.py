60ecbbdedb5f51d09936d9d3f41f72076695ca04
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-01-24 14:09:32 +0700

    change cam to use socket io

--- app.py
+++ app.py
@@ -1,71 +1,94 @@
+import base64
+from io import StringIO, BytesIO
 from flask import Flask, render_template, request, Response
+from flask_socketio import SocketIO, emit
 import cv2
+from PIL import Image, UnidentifiedImageError
+import numpy as np
 
 app = Flask(__name__)
-
+socket_io = SocketIO(app)   
+b64_src = 'data:image/jpg;base64,'
 #LOGIC AND OPEN CV FUNC
-listCam = []
-didCheck = False
-camId = 0
+# listCam = []
+# didCheck = False
+# camId = 0
 
-def change():
-    # print("changing")
-    global camId
-    camId+=1
-    camId %= len(listCam)
+# def change():
+#     # print("changing")
+#     global camId
+#     if(len(listCam > 0)):
+#         camId+=1
+#         camId %= len(listCam)
 
-def generateFrame():
-    try:
-        cap = cv2.VideoCapture(listCam[camId])
-    except IndexError:
-        print(f'cam list len: {len(listCam)}, camId: {camId}')
-        cap = cv2.VideoCapture(0)
-        if(cap.isOpened()):
-            print("ABLE TO OPEN")
-        else:
-            print("cant even run capture 0")
-    while True:
-        success, frame = cap.read()
-        if(not success):
-            break
-        else:
-            '''did tensorflow here'''
+# def generateFrame():
+#     try:
+#         cap = cv2.VideoCapture(listCam[camId])
+#     except IndexError:
+#         print(f'cam list len: {len(listCam)}, camId: {camId}')
+#         cap = cv2.VideoCapture(0)
+#         if(cap.isOpened()):
+#             print("ABLE TO OPEN")
+#         else:
+#             print("cant even run capture 0")
+#     while True:
+#         success, frame = cap.read()
+#         if(not success):
+#             break
+#         else:
+#             '''did tensorflow here'''
             
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
+#             ret, buffer = cv2.imencode('.jpg', frame)
+#             frame = buffer.tobytes()
 
-            yield(b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+#             yield(b'--frame\r\n'
+#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-
+#=================================================WEB WEB WEB WEB=====================
 
 @app.route("/")
 def home():
-    global didCheck
-    if(not didCheck):
-        for i in range(0, 10):
-            cap = cv2.VideoCapture(i)
-            if(not cap.isOpened()):
-                continue
-            listCam.append(i)
+    # global didCheck
+    # if(not didCheck):
+    #     didCheck = True
+    #     for i in range(0, 10):
+    #         cap = cv2.VideoCapture(i)
+    #         if(not cap.isOpened()):
+    #             continue
+    #         listCam.append(i)
 
     # print(listCam)
     return render_template("home.html")
 
 @app.route("/detect/", methods=["POST", "GET"])
 def detect():
-    if(request.method=='POST'):
-        if("changeCam" in request.form):
-            change()
+    # if(request.method=='POST'):
+    #     if("changeCam" in request.form):
+    #         # change()
+    #         pass
     return render_template("detect.html")
 
 
-@app.route('/video')
-def video():
-    return Response(generateFrame(),mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video')
+# def video():
+#     return Response(image(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+@socket_io.on('image')
+def image(data_image):
+    sbuf = StringIO()
+    sbuf.write(data_image)
+    b = BytesIO(base64.b64decode(data_image))
+    pimg = Image.open(b)
+    
+    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+    # frame = cv2.flip(frame, 1)
+    imgencode = cv2.imencode('.jpg', frame)[1]
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    emit('response_back', f'{b64_src}{stringData}')
+    
 
 
 if __name__ == "__main__":
-    # getAvailCam()
     app.run(debug=True)
 
--- static/cam.js
+++ static/cam.js
@@ -1,3 +1,4 @@
+//Camera Triggers Util
 const stopCamButton = document.getElementById("stop-cam");
 if(!!stopCamButton){
     stopCamButton.addEventListener('click', function(){
@@ -7,13 +8,95 @@ if(!!stopCamButton){
     })
 }
 
-
 const startCamButton = document.getElementById("start-cam");
 if(!!startCamButton){
     startCamButton.addEventListener('click', function(){
         // alert("starting camera");
         location.href = "/detect";
     })
+}
+
+//Video Function
+/**
+ * Captures a image frame from the provided video element.
+ *
+ * @param {Video} video HTML5 video element from where the image frame will be captured.
+ * @param {Number} scaleFactor Factor to scale the canvas element that will be return. This is an optional parameter.
+ *
+ * @return {Canvas}
+ */
+ function capture(video, scaleFactor) {
+    if(scaleFactor == null){
+        scaleFactor = 1;
+    }
+    var w = video.videoWidth * scaleFactor;
+    var h = video.videoHeight * scaleFactor;
+    var canvas = document.createElement('canvas');
+        canvas.width  = w;
+        canvas.height = h;
+    var ctx = canvas.getContext('2d');
+        ctx.drawImage(video, 0, 0, w, h);
+    return canvas;
+}     
+ 
+// /**
+//  * Invokes the <code>capture</code> function and attaches the canvas element to the DOM.
+//  */
+// function shoot(){
+//     var video  = document.getElementById(videoId);
+//     var output = document.getElementById('output');
+//     var canvas = capture(video, scaleFactor);
+//         canvas.onclick = function(){
+//             window.open(this.toDataURL());
+//         };
+//     snapshots.unshift(canvas);
+//     output.innerHTML = '';
+//     for(var i=0; i<4; i++){
+//         output.appendChild(snapshots[i]);
+//     }
+// }
+
+//===========================MAIN FUNC=======================
+//make a socket object
+var socket = io('http://127.0.0.1:5000');
+socket.on('connect', function(){
+    console.log("Connected...!", socket.connected)
+});
+
+//get video element
+const video_element = document.querySelector("#videoElement");
+const image_id = document.querySelector('#camera-output');
+
+// Set constraints for the video stream
+var constraints = { video: { facingMode: "user" }, audio: false };
+// Access the device camera and stream to cameraView
+function cameraStart() {
+    navigator.mediaDevices
+        .getUserMedia(constraints)
+        .then(function(stream) {
+        // track = stream.getTracks()[0];
+        video_element.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.error("Oops. Something is broken.", error);
+    });
+}
+if(!!video_element){
+    video_element.style.display = "none";
+    cameraStart();
+    const FPS = 20;
+    const type = "image/webp";
+    //ready to process
+    setInterval(function(){
+        let frame = capture(video_element, 0.5);
+        let data = frame.toDataURL(type);
+        data = data.replace('data:' + type + ';base64,', '');
+        socket.emit('image', data);
+    }, 1000/FPS);
+
+    socket.on('response_back', function(image){        
+        image_id.src = image;
+    });
 }
 
 
